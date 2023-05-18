152f909196e066f46716b4b5a985dedfed069253
hgjinkim <hgjinkim@gmail.com>
2023-05-18 17:21:27 +0900

    add tour route recommadation api

--- location.js
+++ location.js
@@ -26,7 +26,6 @@ router.get("/tour", (req, res) => {
 
 router.get("/restaurant", (req, res) => {
   const options = req.query;
-  console.log(options);
   firestore
     .getData("음식점", options)
     .then((result) => {
@@ -46,5 +45,43 @@ router.get("/restaurant", (req, res) => {
       res.status(404).send(error);
     });
 });
+
+router.get("/recommand-route", (req, res) => {
+  const options = req.query;
+  firestore
+    .getData(options.tourType, options)
+    .then((result) => {
+      let jsonResult = [];
+      let randomList = [];
+
+      result.forEach((doc) => {
+        const temp = doc.data();
+        jsonResult.push(temp);
+      });
+
+      shuffle(jsonResult);
+
+      for (let i = 0; i < 5; i++) {
+        randomList[i] = jsonResult[i];
+      }
+
+      res.status(200).send(randomList);
+      console.log("recommand-tour Info Sended!");
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(404).send(error);
+    });
+});
+
+function shuffle(array) {
+  for (let index = array.length - 1; index > 0; index--) {
+    const randomPosition = Math.floor(Math.random() * (index + 1));
+
+    const temporary = array[index];
+    array[index] = array[randomPosition];
+    array[randomPosition] = temporary;
+  }
+}
 
 module.exports = router;
